f0e51e94f4b7ca924c3ff79e790cc358f419e414
Rani Giro <raniking15@gmail.com>
2024-06-28 10:33:54 +0300

    Feature: Added search-in-catalog functionality

--- JS/handleSearch.js
+++ JS/handleSearch.js
@@ -1,5 +1,31 @@
-
-function handleSearchFromHome(){
-    const input = document.getElementById('search_input');
+window.onload = () =>{
+    if('true' === sessionStorage.getItem('input-value-available')){
+        insertInputData();
+    }
 
 }
+
+export function handleSearchFromHome(){
+    const HomeSearchBar = document.getElementById('Search-bar-modal');
+    sessionStorage.setItem('input-value',HomeSearchBar.value);
+    sessionStorage.setItem('input-value-available','true');
+    window.location.href = 'catalog.html';
+
+    
+}
+
+function insertInputData(){
+    const newInputBar = document.getElementById('search_input');
+    newInputBar.value = sessionStorage.getItem('input-value');
+    newInputBar.focus();
+    sessionStorage.removeItem('input-value-available');
+    sessionStorage.removeItem('input-value');
+
+}
+
+if(window.location.href == 'http://127.0.0.1:5501/index.html'){
+    document.getElementById('search-btn-modal').addEventListener('click',()=>{
+    handleSearchFromHome();
+})
+}
+
