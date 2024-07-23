99172cab2f4df963fcc91cf0d6fb2a292b2b0217
Rani Giro <raniking15@gmail.com>
2024-07-23 19:25:48 +0300

    Bug Fix: changed onclick to event listner.

--- JS/HomePageHandler.js
+++ JS/HomePageHandler.js
@@ -1,4 +1,8 @@
 
-document.getElementById('explore-btn').addEventListener('click',()=>{
+document.getElementById('explore-btn').addEventListener('click', () => {
     window.location.href = 'catalog.html'
+})
+
+document.getElementById('simulation-card-btn').addEventListener('click', () => {
+    window.location.href = 'simulations.html'
 })
